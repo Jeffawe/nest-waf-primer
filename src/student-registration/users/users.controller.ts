f178b8e794acc16c9352ecb219311f0869db4cca
jeffawe <jeffery.ozoekwe-awagu@pau.edu.ng>
2021-02-14 15:37:31 +0100

    Addition to User controller

--- src/student-registration/users/users.controller.ts
+++ src/student-registration/users/users.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, Render } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -30,5 +30,10 @@ export class UsersController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
+  }
+
+  @Get('create')
+  @Render('users/create-user.html')
+  createForm() {
   }
 }
